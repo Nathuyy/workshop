6be6853bbd5e0fcfda9d6851b288f349c20a0cfa
nathy <nathalia.cericatto@gmail.com>
2024-11-13 09:45:51 -0300

    finalizando o projeto

--- src/main/java/com/projetos/curso/service/UserService.java
+++ src/main/java/com/projetos/curso/service/UserService.java
@@ -2,7 +2,12 @@ package com.projetos.curso.service;
 
 import com.projetos.curso.model.User;
 import com.projetos.curso.repository.UserRepository;
+import com.projetos.curso.service.exceptions.DatabaseException;
+import com.projetos.curso.service.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,5 +26,35 @@ public class UserService {
     public User findById(Long id){
         Optional<User> user = userRepository.findById(id);
         return user.orElse(null);
+    }
+
+    public User insert(User obj){
+        return userRepository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public User update(Long id, User obj) {
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
